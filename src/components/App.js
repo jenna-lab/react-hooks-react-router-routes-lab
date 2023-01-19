14f426c81f6b7e255b5a5aa9b3bfe8147d5e1f03
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-19 10:46:58 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
 import Movies from "./Movies";
+import Directors from "./Directors";
+import Actors from "./Actors";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Route exact path="/" element={Home} />
+      <Route path="/movies" element={Movies} />
+      <Route path="/directors" element={Directors} />
+      <Route path="/actors" element={Actors} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+function NavBar({ link }) {
+  return (
+    <nav>
+      <NavLink exact to="/" activeClassName="active-link">
+        Home
+      </NavLink>
+      <NavLink to="/movies" activeClassName="active-link">
+        Movies
+      </NavLink>
+      <NavLink to="/directors" activeClassName="active-link">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" activeClassName="active-link">
+        Actors
+      </NavLink>
+    </nav>
+  );
 }
 
 export default NavBar;
